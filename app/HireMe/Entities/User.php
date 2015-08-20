c38bf43a1278f26927be8a56b1e10d91236f89e6
chrodme <chrodme@hotmail.com>
2015-08-19 22:42:24 -0500

    Agregados cambios de las clases 4

--- app/HireMe/Entities/User.php
+++ app/HireMe/Entities/User.php
@@ -4,8 +4,8 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
-
-class User extends Eloquent implements UserInterface, RemindableInterface {
+//use Illuminate\Database\Eloquent;
+class User extends \Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
 
