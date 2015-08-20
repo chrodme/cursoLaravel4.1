d2f91059d3541d544bb41e0ced03f9309d5b1eee
chrodme <chrodme@hotmail.com>
2015-08-19 19:36:40 -0500

    commit iniciado

---
+++ app/database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+//use Faker\Factory as Faker;
+
+class CategoryTableSeeder extends Seeder {
+
+	public function run()
+	{
+		Category::create([
+			'id'		=> 1,
+			'name'		=> 'Backend developers',
+			'slug'		=> 'Backend-developers'
+		]);
+
+		Category::create([
+		'id'		=> 2,
+		'name'		=> 'Fronten developers',
+		'slug'		=> 'Fronten-developers'
+		]);
+
+		Category::create([
+		'id'		=> 3,
+		'name'		=> 'Designer',
+		'slug'		=> 'Designer'
+]		);
+	}
+
+}
